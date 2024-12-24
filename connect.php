3d03ead856acf2d995367be67982a1bdf438f5ec
Anand <anandkumar.kumar190@gmail.com>
2024-12-24 19:35:41 +0530

    update area page .

--- connect.php
+++ connect.php
@@ -4,13 +4,13 @@
    date_default_timezone_set('Asia/Kolkata');
 
 	define('DB_HOST','localhost');
-// /*<<<<<<< HEAD*/
- 	define('DB_USER','root');
- 	define('DB_PASS',''); 
-// /*=======*/
-// 	define('DB_USER','vivandgo_cart');//vivandgo_cart
-// 	define('DB_PASS','cartroute#123456'); //cartroute#123456
-// /*>>>>>>> 33844e7bad3ffdc0811aed4112ae5646b033ba4f*/
-	define('DB_NAME','cartroot');
+/*<<<<<<< HEAD*/
+	define('DB_USER','vivanwmp_cart');
+	define('DB_PASS','Anand@1234'); 
+/*=======*/
+	define('DB_USER','vivandgo_cart');//vivandgo_cart
+	define('DB_PASS','cartroute#123456'); //cartroute#123456
+/*>>>>>>> 33844e7bad3ffdc0811aed4112ae5646b033ba4f*/
+	define('DB_NAME','vivanwmp_cartroute');
 	$con=mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME) or die('Unable to connect');
  ?>
